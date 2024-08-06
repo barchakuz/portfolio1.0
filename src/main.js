8aeb76ee5bc1f74ad920b3d8d7f3c8b25074a4ab
Barchakuz <behroze.barcha@gmail.com>
2024-08-06 12:11:14 +0500

    CV-Resume added

--- src/main.js
+++ src/main.js
@@ -9,9 +9,27 @@ import '../style/components/contact.css'
 import '../style/components/footer.css'
 import '../style/components/mobile-nav.css'
 import mobileNav from '../utils/mobile-nav'
-import resume from "../public/doc/CV-W1-Behroze.pdf"
 import modeSwtich from '../src/mode.js'
 import lazyLoad from '../utils/lazy-loading.js'
+
 mobileNav()
 modeSwtich()
 lazyLoad()
+
+
+const addDownloadListener = (selector, filePath, fileName) => {
+    const elements = document.querySelectorAll(selector);
+    elements.forEach((element) => {
+        element.addEventListener('click', () => {
+            const link = document.createElement('a');
+            link.href = filePath;
+            link.download = fileName;
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+        });
+    });
+};
+
+addDownloadListener('.btnCV', '/doc/CV.pdf', 'CV-Mern-Behroze.pdf');
+addDownloadListener('.btnResume', '/doc/Resume.pdf', 'Resume-Mern-Behroze.pdf');
